0068669a987d8041aff166d0150ff6e223086bf6
Lisa-Ni <niklasson.lisa@gmail.com>
2025-09-30 11:53:01 +0200

    frontend setup and basic code

---
+++ frontend/shui/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BoardPage from './Pages/BoardPage/BoardPage.jsx';
+
+function App() {
+	return (
+		<div className="app">
+			<BoardPage />
+		</div>
+	);
+}
+
+export default App;
